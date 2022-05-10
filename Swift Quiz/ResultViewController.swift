48da5ad0aa38cbf31af4078bb30a252bc957803b
Matheus Oliveira <cruz.matheus99@gmail.com>
2022-05-10 15:39:42 -0300

    Aplicativo finalizado.

---
+++ Swift Quiz/ResultViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultViewController.swift
+//  Swift Quiz
+//
+//  Created by Matheus Oliveira on 10/05/22.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var lbAnswered: UILabel!
+    @IBOutlet weak var lbCorrect: UILabel!
+    @IBOutlet weak var lbWrong: UILabel!
+    @IBOutlet weak var lbScore: UILabel!
+    
+    var totalCorrectAnswers: Int = 0
+    var totalAnswers: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lbAnswered.text = "Perguntas respondidas: \(totalAnswers)"
+        lbCorrect.text = "Perguntas corretas: \(totalCorrectAnswers)"
+        lbWrong.text = "Perguntas erradas: \(totalAnswers - totalCorrectAnswers)"
+        
+        let score = totalCorrectAnswers*100/totalAnswers
+        lbScore.text = "\(score)%"
+    }
+
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
